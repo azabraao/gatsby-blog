dc256a3102a068a707aacf4c80621d53e917ad67
azabraao <az.abraao@gmail.com>
2021-01-23 10:20:00 -0300

    add AniLink and config link transitions

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+
 import Avatar from "../Avatar"
 import * as S from "./styled"
 
@@ -22,7 +23,7 @@ const Profile = () => {
 
   return (
     <S.ProfileWrapper>
-      <S.ProfileLink>
+      <S.ProfileLink to="/" cover direction="left" bg="#16202c" duration={0.6}>
         <Avatar />
         <S.ProfileAuthor>
           {title}
