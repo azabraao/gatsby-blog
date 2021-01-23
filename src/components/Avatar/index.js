5219f0c208a10baa7fa5819b468d97f9453f4620
azabraao <az.abraao@gmail.com>
2021-01-23 09:46:56 -0300

    add responsivity

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,27 +1,24 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+
 import * as S from "./styled"
 
 const Avatar = () => {
-  const { avatarImage } = useStaticQuery(graphql`
-    query {
-      avatarImage: file(relativePath: { eq: "profile-photo.jpeg" }) {
-        childImageSharp {
-          fluid(maxWidth: 60, maxHeight: 60) {
-            ...GatsbyImageSharpFluid_tracedSVG
+  const { avatarImage } = useStaticQuery(
+    graphql`
+      query {
+        avatarImage: file(relativePath: { eq: "profile-photo.jpeg" }) {
+          childImageSharp {
+            fluid(maxWidth: 60) {
+              ...GatsbyImageSharpFluid_tracedSVG
+            }
           }
         }
       }
-    }
-  `)
-
-  return (
-    <S.AvatarWrapper
-      fluid={avatarImage.childImageSharp.fluid}
-      className="foo-bar"
-      style={{ width: "60px" }}
-    />
+    `
   )
+
+  return <S.AvatarWrapper fluid={avatarImage.childImageSharp.fluid} />
 }
 
 export default Avatar
--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -1,14 +1,22 @@
 import styled from "styled-components"
+import media from "styled-media-query"
 
 export const LayoutWrapper = styled.section`
   display: flex;
+
+  ${media.lessThan("large")`
+    flex-direction: column;
+  `}
 `
 
 export const LayoutMain = styled.main`
   background: var(--background);
   min-height: 100vh;
-  padding: 0 3.75rem 0 20rem;
   width: 100%;
+
+  ${media.greaterThan("large")`
+    padding: 0 3.75rem 0 20rem;
+  `}
 
   body#grid & {
     grid-template-areas:
